301a9c637288880d31b7f51fdd94d03961ae9973
slcnyagmurnew <atak123selcan@hotmail.com>
2020-12-03 21:43:52 +0300

    up to date

--- Moon_/Entities/Files.cs
+++ Moon_/Entities/Files.cs
@@ -16,14 +16,18 @@ namespace Moon.Entities
         [MaxLength(100)]
         public string Name { get; set; }
         [MaxLength(100)]
+        [Required(ErrorMessage = "Please upload pdf document.")]
         public string FileType { get; set; }
-        [MaxLength]
         public byte[] DataFiles { get; set; }
         public DateTime? CreatedOn { get; set; }
         public string ownerId { get; set; }
+        [Required(ErrorMessage = "Please enter title for document."), MaxLength(100)]
         public string Title { get; set; }
+        [Required(ErrorMessage = "Please select category for document.")]
         public string Category { get; set; }
+        [Required(ErrorMessage = "Please select course code for document.")]
         public string CourseCode { get; set; }
+        [Required(ErrorMessage = "Please enter lecturer for document.")]
         public string Lecturer { get; set; }
         public int Likes { get; set; }
     }
